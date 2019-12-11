ba2b6c22debbaa28860cc3ecbed2b1ee7db6fe55
Kirk Phillips <kirkphillips@Kirks-MacBook-Air.local>
2019-12-10 19:06:03 -0500

    added mobile changes

--- Maxwell.js
+++ Maxwell.js
@@ -17,11 +17,6 @@ canvas.height = 0;
 
 var clientX;
 var clientY;
- 
-canvas.addEventListener('mousemove', function(e) {
-  clientX = e.pageX - this.offsetLeft;
-  clientY = e.pageY - this.offsetTop;
-}, false);
 
 var img = new Image();
 img.src = "images/CokeSignNoColor.jpg";
@@ -38,56 +33,88 @@ img.onload = function() {
   ctx.globalCompositionOperation = "destination-out";
 }
  
-canvas.addEventListener('mousedown', function(e) {
-    ctx.beginPath();
-    ctx.moveTo(clientX, clientY);
- 
-    canvas.addEventListener('mousemove', onPaint, false);
+// Set up mouse events for drawing
+var drawing = false;
+var mousePos = { x:0, y:0 };
+var lastPos = mousePos;
+canvas.addEventListener("mousedown", function (e) {
+        drawing = true;
+  lastPos = getMousePos(canvas, e);
 }, false);
- 
-canvas.addEventListener('mouseup', function() {
-    canvas.removeEventListener('mousemove', onPaint, false);
+canvas.addEventListener("mouseup", function (e) {
+  drawing = false;
 }, false);
- 
-var onPaint = function(e) {
-  ctx.globalAlpha = 1;
-    ctx.lineTo(clientX, clientY);
+canvas.addEventListener("mousemove", function (e) {
+  mousePos = getMousePos(canvas, e);
+}, false);
+
+// Get the position of the mouse relative to the canvas
+function getMousePos(canvasDom, mouseEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: mouseEvent.clientX - rect.left,
+    y: mouseEvent.clientY - rect.top
+  };
+}
+
+// Get a regular interval for drawing to the screen
+window.requestAnimFrame = (function (callback) {
+  return window.requestAnimationFrame || 
+     window.webkitRequestAnimationFrame ||
+     window.mozRequestAnimationFrame ||
+     window.oRequestAnimationFrame ||
+     window.msRequestAnimaitonFrame ||
+     function (callback) {
+  window.setTimeout(callback, 1000/60);
+     };
+})();
+
+// Draw to the canvas
+function renderCanvas() {
+  if (drawing) {
+    ctx.moveTo(lastPos.x, lastPos.y);
+    ctx.lineTo(mousePos.x, mousePos.y);
     ctx.stroke();
-};
+    lastPos = mousePos;
+  }
+}
+
+// Allow for animation
+(function drawLoop () {
+  requestAnimFrame(drawLoop);
+  renderCanvas();
+})();
 
 // Set up touch events for mobile, etc
 canvas.addEventListener("touchstart", function (e) {
   mousePos = getTouchPos(canvas, e);
-  var touch = e.touches[0];
-  var mouseEvent = new MouseEvent("mousedown", {
-    clientX: touch.clientX,
-    clientY: touch.clientY
-  });
-  canvas.dispatchEvent(mouseEvent);
-}, false);
-
-canvas.addEventListener("touchend", function (e) {
-  var mouseEvent = new MouseEvent("mouseup", {});
-  canvas.dispatchEvent(mouseEvent);
-}, false);
-
-canvas.addEventListener("touchmove", function (e) {
-  var touch = e.touches[0];
-  var mouseEvent = new MouseEvent("mousemove", {
-  clientX: touch.clientX,
-  clientY: touch.clientY
+var touch = e.touches[0];
+var mouseEvent = new MouseEvent("mousedown", {
+clientX: touch.clientX,
+clientY: touch.clientY
 });
-
+canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+var mouseEvent = new MouseEvent("mouseup", {});
+canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+var touch = e.touches[0];
+var mouseEvent = new MouseEvent("mousemove", {
+clientX: touch.clientX,
+clientY: touch.clientY
+});
 canvas.dispatchEvent(mouseEvent);
 }, false);
 
 // Get the position of a touch relative to the canvas
 function getTouchPos(canvasDom, touchEvent) {
-  var rect = canvasDom.getBoundingClientRect();
-  return {
-    x: touchEvent.touches[0].clientX - rect.left,
-    y: touchEvent.touches[0].clientY - rect.top
-  };
+var rect = canvasDom.getBoundingClientRect();
+return {
+x: touchEvent.touches[0].clientX - rect.left,
+y: touchEvent.touches[0].clientY - rect.top
+};
 }
 
 // Prevent scrolling when touching the canvas
@@ -106,26 +133,3 @@ document.body.addEventListener("touchmove", function (e) {
     e.preventDefault();
   }
 }, false);
-
-// Image color/sections functions
-function updateColor() {
-  var dropdown = document.getElementById('color-dropdown');
-  ctx.strokeStyle = dropdown.options[dropdown.selectedIndex].value;
-}
-
-function updateSize() {
-  var sizedropdown = document.getElementById('size-dropdown');
-  ctx.lineWidth = sizedropdown.options[sizedropdown.selectedIndex].value;
-}
-
-function updateOpacity() {
-  var opacitydropdown = document.getElementById('opacity-dropdown');
-  var colorCanvas = document.getElementById('myCanvas');
-  colorCanvas.style.opacity = opacitydropdown.options[opacitydropdown.selectedIndex].value;
-}
-
-function toggleColor(event) {
-  var image = event.target;
-  image.classList.toggle("withColor");
-}
-
